9800cc360b25e9ce946d1e0c5b500f34db169570
AvaAvarai <AvaAvarai@gmail.com>
2020-12-03 14:51:01 -0800

    Add piston graphics/opengl_graphics

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,26 @@
 extern crate glutin_window;
 extern crate piston;
 
+extern crate graphics;
+extern crate opengl_graphics;
+
 use glutin_window::GlutinWindow;
 use piston::WindowSettings;
 use piston::event_loop::{EventSettings, Events};
+use piston::RenderEvent;
+use opengl_graphics::{GlGraphics, OpenGL};
 
 fn main() {
+    let opengl = OpenGL::V3_2;
     let settings = WindowSettings::new("soko-test", [512; 2]).exit_on_esc(true);
     let mut window: GlutinWindow = settings.build().expect("Error creating window");
-
+    let mut gl = GlGraphics::new(opengl);
+   
     let mut events = Events::new(EventSettings::new());
-    while let Some(_) = events.next(&mut window) {
-        // Main Loop
+    while let Some(e) = events.next(&mut window) {
+        if let Some(_) = e.render_args() {
+            graphics::clear([0.0, 0.0, 1.0, 1.0], &mut gl);
+            // Main Loop
+        }
     }
-
 }
