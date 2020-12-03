dd3bf0ae0f8e27d09df28a546d06b6b081286e3f
AvaAvarai <AvaAvarai@gmail.com>
2020-12-03 08:59:22 -0800

    Add piston/glutin_window and display window

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-// Hello World test
+//! A test at creating a Sokoban style game in Rust.
+extern crate glutin_window;
+extern crate piston;
+
+use glutin_window::GlutinWindow;
+use piston::WindowSettings;
+use piston::event_loop::{EventSettings, Events};
+
 fn main() {
-    print!("Hello World!")
+    let settings = WindowSettings::new("soko-test", [512; 2]).exit_on_esc(true);
+    let mut window: GlutinWindow = settings.build().expect("Error creating window");
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(_) = events.next(&mut window) {
+        // Main Loop
+    }
+
 }
