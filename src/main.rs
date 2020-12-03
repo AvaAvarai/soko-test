ca2317aa1a7fecab223af80c580e1f482ff18f57
AvaAvarai <AvaAvarai@gmail.com>
2020-12-03 15:39:33 -0800

    Keyboard input and player movement

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ extern crate opengl_graphics;
 use glutin_window::GlutinWindow;
 use piston::WindowSettings;
 use piston::event_loop::{EventSettings, Events};
-use piston::RenderEvent;
+use piston::input::{Button, ButtonState, Key};
+use piston::{ButtonEvent, RenderEvent};
 use opengl_graphics::{GlGraphics, GlyphCache, OpenGL, TextureSettings};
 use graphics::character::CharacterCache;
 
@@ -19,6 +20,9 @@ fn main() {
     let mut gl = GlGraphics::new(opengl);
     let ref mut glyphs = GlyphCache::new("assets/PressStart2P-Regular.ttf", (), TextureSettings::new()).expect("Error loading font");
    
+    let mut player_x = 224.0;
+    let mut player_y = 224.0;
+
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
         if let Some(r) = e.render_args() {
@@ -28,10 +32,22 @@ fn main() {
                     graphics::Image::new_color([0.0, 0.0, 0.0, 1.0]).draw(
                         glyphs.character(32, '@').unwrap().texture,
                         &c.draw_state,
-                        c.transform.trans(224.0, 224.0),
+                        c.transform.trans(player_x, player_y),
                         g
                     );
             });
+        }
+
+        if let Some(k) = e.button_args() {
+            if k.state == ButtonState::Press {
+                match k.button {
+                    Button::Keyboard(Key::Up) => player_y -= 32.0,
+                    Button::Keyboard(Key::Down) => player_y += 32.0,
+                    Button::Keyboard(Key::Left) => player_x -= 32.0,
+                    Button::Keyboard(Key::Right) => player_x += 32.0,
+                    _ => (),
+                }
+            }
         }
     }
 } 
